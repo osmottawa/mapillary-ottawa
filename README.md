ef724c46ea412887923cf3c46b56001c9b958249
Denis <carriere.denis@gmail.com>
2017-04-29 14:26:30 -0400

    Initialize library

--- README.md
+++ README.md
@@ -4,3 +4,15 @@ Mapillary Images & Sequences for the City of Ottawa.
 
 Before using this data, one must agree with the [Terms and Conditions](https://www.mapillary.com/terms) of Mapillary.
 
+## Install
+
+```bash
+$ npm install
+```
+
+## Updates
+
+```bash
+$ npm start
+$ npm run git-commit
+```
---
+++ index.js
@@ -0,0 +1,110 @@
+#!/usr/bin/env node
+const fs = require('fs')
+const path = require('path')
+const load = require('load-json-file')
+const meow = require('meow')
+const range = require('lodash.range')
+const write = require('write-json-file')
+const axios = require('axios')
+const mkdirp = require('mkdirp')
+const Protobuf = require('pbf')
+const mercator = require('global-mercator')
+const slippyGrid = require('slippy-grid')
+const slippyTile = require('slippy-tile')
+const {VectorTile} = require('vector-tile')
+const {featureCollection} = require('@turf/helpers')
+const ProgressBar = require('progress')
+
+const cli = meow(`
+    Usage:
+      $ node index.js <geojson>
+
+    Options:
+      --verbose        Output verbose messages on internal operations
+
+    Examples:
+      $ node index.js "extents/ottawa.geojson"
+`, {
+  alias: {v: 'verbose'},
+  boolean: 'verbose'
+})
+
+/**
+ * Required Flags
+ */
+if (cli.input.length === 0) throw new Error('<geojson> is required')
+if (!fs.existsSync(cli.input[0])) throw new Error('<geojson> filepath does not exist')
+const geojson = load.sync(cli.input[0])
+
+async function main () {
+  const total = slippyGrid.count(geojson, 14, 14)
+  const bar = new ProgressBar('  downloading [:bar] :percent (:current/:total)', {
+    total,
+    width: 20
+  })
+
+  // Iterate over each tile
+  const grid = slippyGrid.single(geojson, 14, 14)
+  while (true) {
+    const {value, done} = grid.next()
+    if (done) break
+    bar.tick()
+    const tile = mercator.tileToGoogle(value)
+    const data = await requestVectorTile(tile)
+
+    // Store Results
+    if (data) {
+      const images = vectorTileToGeoJSON(data, 'mapillary-images', tile)
+      const sequences = vectorTileToGeoJSON(data, 'mapillary-sequences', tile)
+
+      const [x, y, z] = tile.map(v => String(v))
+
+      // Save Images
+      mkdirp(path.join(__dirname, 'images', z, x), () => {
+        write.sync(path.join(__dirname, 'images', z, x, y + '.geojson'), images)
+      })
+      // Save Sequences
+      mkdirp(path.join(__dirname, 'sequences', z, x), () => {
+        write.sync(path.join(__dirname, 'sequences', z, x, y + '.geojson'), sequences)
+      })
+    }
+  }
+}
+main()
+
+/**
+ * Request VectorTile
+ *
+ * @param {Tile} tile [x, y, z]
+ * @returns {Buffer} Vector Tile Buffer
+ */
+async function requestVectorTile (tile) {
+  const url = slippyTile(tile, 'https://d25uarhxywzl1j.cloudfront.net/v0.1/{z}/{x}/{y}.mvt')
+
+  // Request vector tile from Mapillary
+  return axios.get(url, {responseType: 'arraybuffer'})
+    .then(response => response.data, error => error)
+}
+
+/**
+ * Convert VectorTile to GeoJSON
+ *
+ * @param {Buffer} data Raw data
+ * @param {string} layer layer to extract
+ * @param {Tile} tile [x, y, z]
+ * @returns {FeatureCollection} GeoJSON FeatureCollection
+ */
+function vectorTileToGeoJSON (data, layer, tile) {
+  const [x, y, z] = tile
+  const results = featureCollection([])
+  const vt = new VectorTile(new Protobuf(data))
+  const result = vt.layers[layer]
+
+  if (result) {
+    range(result.length).forEach(i => {
+      const feature = result.feature(i).toGeoJSON(x, y, z)
+      results.features.push(feature)
+    })
+  }
+  return results
+}
